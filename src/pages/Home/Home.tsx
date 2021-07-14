242cc217d84149953cb5f059ae34adbefe7f8946
pedro-drosa <pedro_drosa@hotmail.com>
2021-07-14 10:13:20 -0300

    feature: new section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import {Input} from '../../components/Input/Input';
 import {Button} from '../../components/Button/Button';
 
 import backgroundImg from '../../assets/images/background.jpg'; 
+import userImg from '../../assets/images/user.jpg'; 
 
 export function Home() {
   const backgroundStyle = {
@@ -15,6 +16,7 @@ export function Home() {
   }
 
   return(
+  <>
     <main style={backgroundStyle}>
       <div className='container'>
         <header>
@@ -28,5 +30,18 @@ export function Home() {
         </header>
       </div>
     </main>
+    <aside>
+      <section>
+        <div className="photo">
+          <img src={userImg} alt="user" />
+        </div>
+        <div className="photo-content">
+          <h1>GET AMAZING RESULT</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+        </div>
+      </section>
+    </aside>
+  </>
   )
 }
